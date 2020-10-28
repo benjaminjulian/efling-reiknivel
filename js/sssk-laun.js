f131b8a04431d55ebdb678e9224a3edb62266af5
benjaminjulian <benjaminjulian@protonmail.com>
2020-10-28 10:39:09 +0000

    SSSK

---
+++ js/sssk-laun.js
@@ -0,0 +1,90 @@
+laun = {
+  "Aðstoðarmaður í eldhúsi": {
+    "starfsmat": 325,
+    "lfl": 227,
+    "grunnur": 308199,
+    "tafla": 6157,
+    "leidr": 13500,
+	"annad": 0
+  },
+  "Deildarstjóri C": {
+    "starfsmat": 489,
+    "lfl": 253,
+    "grunnur": 389818,
+    "tafla": 5626,
+    "leidr": 0,
+	"annad": 42500
+  },
+  "Leiðbeinandi 1": {
+    "starfsmat": 335,
+    "lfl": 228,
+    "grunnur": 310973,
+    "tafla": 10043,
+    "leidr": 10500,
+	"annad": 32500
+  },
+  "Leiðbeinandi 1 með stuðning": {
+    "starfsmat": 371,
+    "lfl": 234,
+    "grunnur": 328148,
+    "tafla": 6593,
+    "leidr": 4500,
+	"annad": 32500
+  },
+  "Leiðbeinandi 2": {
+    "starfsmat": 365,
+    "lfl": 233,
+    "grunnur": 325220,
+    "tafla": 6038,
+    "leidr": 6000,
+	"annad": 37500
+  },
+  "Leiðbeinandi 2 með stuðning": {
+    "starfsmat": 391,
+    "lfl": 237,
+    "grunnur": 337088,
+    "tafla": 8311,
+    "leidr": 0,
+	"annad": 37500
+  },
+  "Leikskólaliði": {
+    "starfsmat": 378,
+    "lfl": 235,
+    "grunnur": 331102,
+    "tafla": 7156,
+    "leidr": 3000,
+	"annad": 37500
+  },
+  "Leikskólaliði með stuðning": {
+    "starfsmat": 391,
+    "lfl": 237,
+    "grunnur": 337088,
+    "tafla": 8311,
+    "leidr": 0,
+	"annad": 37500
+  },
+  "Starfsmaður 2": {
+    "starfsmat": 322,
+    "lfl": 226,
+    "grunnur": 305450,
+    "tafla": 8906,
+    "leidr": 13500,
+	"annad": 32500
+  },
+  "Starfsmaður 2 með stuðning": {
+    "starfsmat": 358,
+    "lfl": 232,
+    "grunnur": 322320,
+    "tafla": 8938,
+    "leidr": 6000,
+	"annad": 32500
+  },
+  "Yfirmaður mötuneytis leikskóla.": {
+    "starfsmat": 453,
+    "lfl": 246,
+    "grunnur": 365395,
+    "tafla": 10031,
+    "leidr": 0,
+	"annad": 0
+  }
+}
